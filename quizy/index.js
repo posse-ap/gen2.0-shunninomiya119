3c3524c9cbd13be8563d1c62730e21bdc9668724
Shun Ninomiya <n119shun@keio.jp>
2021-07-26 19:35:04 +0900

    配列とループを使ってjs上でhtmlの表示をできるようにした

--- quizy/index.js
+++ quizy/index.js
@@ -1,8 +1,4 @@
 'use strict';
-const correct_answer_button = document.getElementById('correct_answer');
-const incorrect_answer_button = document.getElementById('incorrect_answer');
-const incorrect_answer_button2 = document.getElementById('incorrect_answer2');
-const result_divided = document.getElementById('result_area');
 
 let choices = [
     ['たかなわ', 'たかわ', 'こうわ'],
@@ -19,7 +15,7 @@ let choices = [
 ];
 
 let images = [
-    'https://d1khcm40x1j0f.cloudfront.net/quiz/34d20397a2a506fe2c1ee636dc011a07.png',,
+    'https://d1khcm40x1j0f.cloudfront.net/quiz/34d20397a2a506fe2c1ee636dc011a07.png',
     'https://d1khcm40x1j0f.cloudfront.net/quiz/512b8146e7661821c45dbb8fefedf731.png',
     'https://d1khcm40x1j0f.cloudfront.net/quiz/ad4f8badd896f1a9b527c530ebf8ac7f.png',
     'https://d1khcm40x1j0f.cloudfront.net/quiz/ee645c9f43be1ab3992d121ee9e780fb.png',
@@ -32,103 +28,63 @@ let images = [
 
 ];
 
-let main;
- main +='<div id="display_area" class = "container_wrapper">'
- +'<h3 class = "size question_underline">1.この地名はなんて読む？</h3>'
- +'<img src= alt="">'
- +'<p> <button id="correct_answer"></button></p>'
- +'<p> <button id="incorrect_answer"></button> </p>'
- +'<p> <button id="incorrect_answer2"></button> </p>'
- +'<div id="result_area"></div>'
- +'</div>'
-
- const loop = document.getElementById('loop');
- loop.innerHTML = main
-
-
-
-
 for (let i = 0; i < choices.length; i++) {
     
-    correct_answer_button.innerText = 
-    
-}
+    let display_area = '';
+    display_area +='<div id="display_area" class = "container_wrapper">'
+    +'<h3 class = "size question_underline">1.この地名はなんて読む？</h3>'
+    +`<img src="${images[i]}">`;
+    for (let k = 0; k < choices[i].length; k++) {
+    display_area +=`<p> <button id="choices_button">${choices[i][k]}</button></p>`
+    };
+    display_area +='<div id="result_area"></div>'
+    +'</div>';
 
-//無名関数を使って結果を表示する
-correct_answer_button.onclick = () => {
-    // 全てのボタンが2回目は押せないようにする
-    correct_answer_button.classList.add('click_invalidation');
-    incorrect_answer_button.classList.add('click_invalidation');
-    incorrect_answer_button2.classList.add('click_invalidation');
+    document.write(display_area);
+    console.log(display_area);
 
-    // ボタンの背景色と文字の色を変える
-    correct_answer_button.style.background='#287dff';
-    correct_answer_button.classList.toggle('font_color');
+};
 
-    // 結果を表示するエリアを作る
-    result_divided.classList.add('result_area');
+const choices_button = document.getElementById('choices_button');
+const result_divided = document.getElementById('result_area');
 
-    // エリア内に文章を表示する
-    const header = document.createElement('h4');
-    header.innerText = '正解！';
-    result_divided.appendChild(header);
-    header.classList.add('answer_underline'); // 正解！に下線をつける
+choices_button.addEventListener('click', my_func);
 
-    const paragraph = document.createElement('p');
-    paragraph.innerText = '正解は「' + correct_answer_button.innerText + '」です！';
-    result_divided.appendChild(paragraph);
-
-}
-
-// 同様にもう二つの選択しに対しても設定
-incorrect_answer_button.onclick = () => {
-    correct_answer_button.classList.add('click_invalidation');
-    incorrect_answer_button.classList.add('click_invalidation');
-    incorrect_answer_button2.classList.add('click_invalidation');
-    
-    incorrect_answer_button.style.background= '#ff5128';
-    incorrect_answer_button.classList.add('font_color');
-    // 正解のボタンも色が変わるようにする
-    correct_answer_button.style.background='#287dff';
-    correct_answer_button.classList.toggle('font_color');
-
-    result_divided.classList.add('result_area');
-
-    const header = document.createElement('h4');
-    header.innerText = '不正解！';
-    result_divided.appendChild(header);
-    header.classList.add('answer_underline');
-
-    const paragraph = document.createElement('p');
-    paragraph.innerText = '正解は「' + correct_answer_button.innerText + '」です！';
-    result_divided.appendChild(paragraph);
- 
-}
-
-incorrect_answer_button2.onclick = () => {
-    correct_answer_button.classList.add('click_invalidation');
-    incorrect_answer_button.classList.add('click_invalidation');
-    incorrect_answer_button2.classList.add('click_invalidation');
-
-    incorrect_answer_button2.style.background= '#ff5128';
-    incorrect_answer_button2.classList.toggle('font_color')
-    correct_answer_button.style.background='#287dff';
-    correct_answer_button.classList.toggle('font_color');
-    
-    result_divided.classList.add('result_area');
-
-    const header = document.createElement('h4');
-    header.innerText = '不正解！';
-    result_divided.appendChild(header);
-    header.classList.add('answer_underline');
-
-    const paragraph = document.createElement('p');
-    paragraph.innerText = '正解は「' + correct_answer_button.innerText + '」です！';
-    result_divided.appendChild(paragraph);
-}
-
-
-
-
-
+function my_func() {
+    if (choices[0][0] || [1][2] || [2][0] || [3][0] || [4][2] || [5][2] || [6][0] || [7][2] || [8][0] [9][0]) {
+        // ボタンの背景色と文字の色を変える
+       choices_button.style.background='#287dff';
+       choices_button.classList.toggle('font_color');
+   
+       // 結果を表示するエリアを作る
+       result_divided.classList.add('result_area');
+   
+       // エリア内に文章を表示する
+       const header = document.createElement('h4');
+       header.innerText = '正解！';
+       result_divided.appendChild(header);
+       header.classList.add('answer_underline'); // 正解！に下線をつける
+   
+       const paragraph = document.createElement('p');
+       paragraph.innerText = '正解は「' + choices_button.innerText + '」です！';
+       result_divided.appendChild(paragraph);   
+   } else {
+       choices_button.style.background= '#ff5128';
+       choices_button.classList.add('font_color');
+       // 正解のボタンも色が変わるようにする
+       choices_button.style.background='#287dff';
+       choices_button.classList.toggle('font_color');
+   
+       result_divided.classList.add('result_area');
+   
+       const header = document.createElement('h4');
+       header.innerText = '不正解！';
+       result_divided.appendChild(header);
+       header.classList.add('answer_underline');
+   
+       const paragraph = document.createElement('p');
+       paragraph.innerText = '正解は「' + choices_button.innerText + '」です！';
+       result_divided.appendChild(paragraph);    
+   };
+};
 
