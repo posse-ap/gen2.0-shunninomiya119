808951a1f1d0f19a0d2205688a50450e050c6697
Shun Ninomiya <n119shun@keio.jp>
2021-08-16 00:21:41 +0900

    シャッフルした後の配列から正解のインデックスを見つけ、それを応用して正解のボタンのidを取ってくることができた

--- quizy/index.js
+++ quizy/index.js
@@ -3,7 +3,7 @@
 let choices = [
     ['たかなわ', 'たかわ', 'こうわ'],
     ['かめいど', 'かめど', 'かめと'],
-    ['おかちまち', 'こうじまち', 'かゆまち'],
+    ['こうじまち', 'おかとまち', 'かゆまち'],
     ['おなりもん', 'ごせいもん', 'おかどもん'],
     ['とどろき', 'たたら', 'たたりき'],
     ['しゃくじい', 'いじい', 'せきこうい'],
@@ -28,15 +28,6 @@ function shuffleArray(arr) {
     return arr;
 };
 
-for (let i = 0; i < choices.length; i++) {
-    shuffleArray(choices[i]);
-};
-
-let after_shuffle_choices = choices
-console.log(after_shuffle_choices);
-const correct_answer = after_shuffle_choices[0].find(element => element === 'たかなわ');
-console.log(correct_answer);
-
 let images = [
     'https://d1khcm40x1j0f.cloudfront.net/quiz/34d20397a2a506fe2c1ee636dc011a07.png',
     'https://d1khcm40x1j0f.cloudfront.net/quiz/512b8146e7661821c45dbb8fefedf731.png',
@@ -51,15 +42,20 @@ let images = [
 ];
 
 for (let i = 0; i < choices.length; i++) {
+    let correct_answer = choices[i][0];
+   
+    shuffleArray(choices[i]);
+
+    let correct_answer_index = choices[i].findIndex(element => element === correct_answer);
     
     let display_area = '';  // for分の中で「display_areaを空の文字列とおく」という処理をしなければ、document.writeした時にdisplay_areaが上書きされて表示内容が１→３→６→１０という具合に増えていってしまう
     display_area +='<div id="display_area" class="container_wrapper">'
     +`<h1 class="size question_underline">${i+1}. この地名はなんて読む？</h1>`
     +`<img src="${images[i]}" class="image">`
     // idは固有なもののため、問題番号ごとの選択肢ボタンと解答ボックスのidを取得できるように、idに問題番号を表す変数iをつける
-    +`<p> <button id="option_button${i}_0" onclick = "my_func(${[i]},0,0)">${choices[i][0]}</button> </p>`
-    +`<p> <button id="option_button${i}_1" onclick = "my_func(${[i]},1,0)">${choices[i][1]}</button> </p>`
-    +`<p> <button id="option_button${i}_2" onclick = "my_func(${[i]},2,0)">${choices[i][2]}</button> </p>`
+    +`<p> <button class="button${i}" id="option_button${i}_0" onclick = "my_func(${[i]},0,${correct_answer_index})">${choices[i][0]}</button> </p>`
+    +`<p> <button class="button${i}" id="option_button${i}_1" onclick = "my_func(${[i]},1,${correct_answer_index})">${choices[i][1]}</button> </p>`
+    +`<p> <button class="button${i}" id="option_button${i}_2" onclick = "my_func(${[i]},2,${correct_answer_index})">${choices[i][2]}</button> </p>`
     +`<div id="answer_box${i}"></div>`
     +'</div>';
 
@@ -69,12 +65,14 @@ for (let i = 0; i < choices.length; i++) {
 
 // my_funcを定義する時の引数は仮引数。実引数の意味がわかるような名前をつける
 function my_func(question_number, option_number, correct_answer_number) {
+    // 選択番号のところをcorrect_answer_numberという変数で表してあげれば、正解のインデックスと同じ選択番号のボタンを正解のボタンとして取ってこれる
+    let correct_button= document.getElementById(`option_button${question_number}_${correct_answer_number}`);
 
-    // 変数iは上のfor文の中でしか参照できないため、代わりとして仮引数としてiを参照しているquestion_numberを使う
-    // 関数内で以下の変数を定義しなければquestion_numberを参照できない
+    // 不正解の時の処理のために、全てのボタンを取ってくる
     let option_button0 = document.getElementById(`option_button${question_number}_0`);
     let option_button1 = document.getElementById(`option_button${question_number}_1`);
     let option_button2 = document.getElementById(`option_button${question_number}_2`);
+
     let answer_box = document.getElementById(`answer_box${question_number}`);
 
     if (option_number === correct_answer_number) {
@@ -82,19 +80,49 @@ function my_func(question_number, option_number, correct_answer_number) {
         option_button0.classList.add('click_invalidation');
         option_button1.classList.add('click_invalidation');
         option_button2.classList.add('click_invalidation');
+
         // 正解の選択肢ボタンの背景色と文字の色を変える
-        option_button0.style.background = '#287dff';
-        option_button0.classList.add('font_color');
+        correct_button.style.background = '#287dff';
+        correct_button.classList.add('font_color');
+
         // 解答を表示するボックスを作る
         answer_box.classList.add('answer_box');
+
         // 解答ボックス内に文章を表示する
         const paragraph1 = document.createElement('p');
         paragraph1.innerText = '正解！';
         answer_box.appendChild(paragraph1);
         paragraph1.classList.add('answer_underline'); // 正解！に下線をつける
-
         const paragraph2 = document.createElement('p');
-        paragraph2.innerText = `正解は「${option_button0.innerText}」です！`;
+        paragraph2.innerText = `正解は「${correct_button.innerText}」です！`;
+        answer_box.appendChild(paragraph2);
+        
+    } else if (option_number === 0) {  // else ifは何回使っても良いため、全ボタンに対して不正解の時の処理を場合分けする。
+        // if分は最初のifが通ったらその後のelseの処理は見ないため、elseifの条件式の中に正解のボタンがあったとしても影響はない
+
+        // 全てのボタンを二度目は押せないようにする
+        option_button0.classList.add('click_invalidation');
+        option_button1.classList.add('click_invalidation');
+        option_button2.classList.add('click_invalidation');
+
+        //  押したボタンの色を間違えた時の色に変える
+        option_button0.style.background = '#ff5128';
+        option_button0.classList.add('font_color');
+
+        // 正解の選択肢ボタンの色も変えてわかりやすくする
+        correct_button.style.background = '#287dff';
+        correct_button.classList.add('font_color');
+
+        // 解答を表示するボックスを見えるようにする
+        answer_box.classList.add('answer_box');
+
+        // 解答ボックス内に文章を表示する
+        const paragraph1 = document.createElement('p');
+        paragraph1.innerText = '不正解！';
+        answer_box.appendChild(paragraph1);
+        paragraph1.classList.add('answer_underline');
+        const paragraph2 = document.createElement('p');
+        paragraph2.innerText = `正解は「${correct_button.innerText}」です！`;
         answer_box.appendChild(paragraph2);
 
     } else if (option_number === 1) {
@@ -102,49 +130,55 @@ function my_func(question_number, option_number, correct_answer_number) {
         option_button0.classList.add('click_invalidation');
         option_button1.classList.add('click_invalidation');
         option_button2.classList.add('click_invalidation');
+
         //  押したボタンの色を間違えた時の色に変える
         option_button1.style.background = '#ff5128';
         option_button1.classList.add('font_color');
+
         // 正解の選択肢ボタンの色も変えてわかりやすくする
-        option_button0.style.background = '#287dff';
-        option_button0.classList.add('font_color');
+        correct_button.style.background = '#287dff';
+        correct_button.classList.add('font_color');
+
         // 解答を表示するボックスを見えるようにする
         answer_box.classList.add('answer_box');
+
         // 解答ボックス内に文章を表示する
         const paragraph1 = document.createElement('p');
         paragraph1.innerText = '不正解！';
         answer_box.appendChild(paragraph1);
         paragraph1.classList.add('answer_underline');
-
         const paragraph2 = document.createElement('p');
-        paragraph2.innerText = `正解は「${option_button0.innerText}」です！`;
+        paragraph2.innerText = `正解は「${correct_button.innerText}」です！`;
         answer_box.appendChild(paragraph2);
-
+    
     } else {
         // 全てのボタンを二度目は押せないようにする
         option_button0.classList.add('click_invalidation');
         option_button1.classList.add('click_invalidation');
         option_button2.classList.add('click_invalidation');
+
         //  押したボタンの色を間違えた時の色に変える
         option_button2.style.background = '#ff5128';
         option_button2.classList.add('font_color');
+
         // 正解の選択肢ボタンの色も変えてわかりやすくする
-        option_button0.style.background = '#287dff';
-        option_button0.classList.add('font_color');
+        correct_button.style.background = '#287dff';
+        correct_button.classList.add('font_color');
+
         // 解答を表示するボックスを作る
         answer_box.classList.add('answer_box');
+
         // 解答ボックス内に文章を表示する
         const paragraph1 = document.createElement('p');
         paragraph1.innerText = '不正解！';
         answer_box.appendChild(paragraph1);
         paragraph1.classList.add('answer_underline');
-
         const paragraph2 = document.createElement('p');
-        paragraph2.innerText = `正解は「${option_button0.innerText}」です！`;
+        paragraph2.innerText = `正解は「${correct_button.innerText}」です！`;
         answer_box.appendChild(paragraph2);
-
     };
 };
+
 
 
 
